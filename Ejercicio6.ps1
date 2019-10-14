500cd83f1e07acb2f3a96066ad6a9e805e7b87fd
TomasSG <tomassanchezgrigioni98@gmail.com>
2019-10-14 13:54:19 -0300

    Se agrego la ayuda con Get-Help de script y se modifico las ayudas de las funciones

--- Ejercicio6.ps1
+++ Ejercicio6.ps1
@@ -1,4 +1,26 @@
-﻿
+﻿<#
+.SYNOPSIS
+    Permita realizar producto escalar y suma de matrices. La entrada de la matriz al script se realizará mediante un archivo de texto plano. 
+    La salida se guardará en otro archivo que se llamará “salida.nombreArchivoEntrada” en el mismo directorio donde se encuentra el script
+.DESCRIPTION
+    Integrantes:
+    Giselle González - 41292355
+    Tomas Grigioni - 41589109
+    Alan Rossi - 37250221
+    Lautaro Costa - 36396983
+    Diego Stanko - 39372117
+
+.PARAMETER Entrada
+    Path del archivo donde esta cargada la matriz.
+.PARAMETER Producto
+    Entero para realizar el producto escalar con la matriz especificada en -Entrada. No se puede usar con -Suma
+.PARAMETER Suma
+    Path del archivo de la matriz a sumar a la indicada en -Enrada. No se puede usarjunto con -Producto
+
+.EXAMPLE
+    Ejercicio6.ps1 -Entrada .\matrizEjemplo.txt -Suma .\matrizEjemploSuma.txt
+#>
+
 
 
 [cmdletbinding(DefaultParameterSetName="Suma")]
@@ -30,7 +52,7 @@ Param (
         Los elementos de una misma fila separados por | y las filas separados por /n.
     Se genera una matriz con las dimensiones necesarias y carga los datos en esa nueva matriz. La función retorna la matriz 
     creada, la cantidad de filas ,y por último, la cantidad de columnas
-.PARAMETER
+.PARAMETER Path
     Dirección de fichero con los datos, no se hara ninguna validación entonces se debe asegurar que la dirección sea correcta 
     previamente al llamado de dicha función
 #>
@@ -60,8 +82,14 @@ function cargarMatriz([String] $path){
     Dado un archivo y una matriz, se escirbe en el archiovo la matriz especeificada. El separadaro entre elementos de la misma fila es el "|" y el separadaor de columnas
     es el salto de línea "\n".
     Si ya existe el fichero donde se va a escribir, el nuevo archivo va a pisar al viejo
-.PARAMETER
-    Dirección de fichero donde escribir, la cantidad de filas que contiene la matriz, la cantidad de columnas y la matriz a cargar.
+.PARAMETER Path
+    Dirección de fichero donde escribir la matriz
+.PARAMETER Mat
+    Matriz a escribir
+.PARAMETER Fil
+    Cantidad de filas de la matriz
+.PARAMETER Col
+    Cantidad de columnas de la matriz
 #>
 function escribirMatriz([String] $path, $mat, [int] $fil, [int] $col){
     if ( Test-Path "$path" -PathType Leaf){
@@ -89,13 +117,18 @@ function escribirMatriz([String] $path, $mat, [int] $fil, [int] $col){
     A partir de las matrices y sus respectivas filas y columnas, se genera una matriz con las dimensiones correspondientes. En la matriz resultado se guarda la suma
     de las otras dos matrices.
     La función retorna la matriz resultado, la cantidad de filas y columnas
-.PARAMETER
-    $matriz1 --> La primera matriz a sumar
-    $fil1 --> La cantidad de filas de la primera matriz
-    $col1 --> La cantidad de columnas de la primera matriz
-    $matriz2 --> La segunda matriz a sumar
-    $fil2 --> La cantidad de filas de la segunda matriz
-    $col2 --> La cantidad de columnas de la segunda matriz
+.PARAMETER Matriz1
+    La primera matriz a sumar
+.PARAMETER Fil1
+    La cantidad de filas de la primera matriz
+.PARAMETER Col1
+    La cantidad de columnas de la primera matriz
+.PARAMETER Matriz2
+    La segunda matriz a sumar
+.PARAMETER Fil2
+    La cantidad de filas de la segunda matriz
+.PARAMETER Col2
+    La cantidad de columnas de la segunda matriz
 #>
 function sumarMatrices($matriz1,[int] $fil1, [int] $col1, $matriz2, [int] $fil2, [int] $col2){
     $matRes = New-Object 'Object[,]' $fil1,$col1
@@ -113,11 +146,14 @@ function sumarMatrices($matriz1,[int] $fil1, [int] $col1, $matriz2, [int] $fil2,
 .DESCRIPTION
     A partir de la matriz dada y el escalar, se genera una nueva matriz con sus respectivas dimensiones donde se guarda el resultado del producto escalar
     La función retorna la matriz resultado, la cantidad de filas y columnas
-.PARAMETER
-   $matriz --> La matriz con la cual se realiza el producto escalar
-   $fil --> Cantidad de filas de la matriz
-   $col --> Cantidad de columnas de la matriz
-   $escalar --> El escalar con el cual se realiza el producto escalar
+.PARAMETER Matriz
+   La matriz con la cual se realiza el producto escalar
+.PARAMETER Fil
+   Cantidad de filas de la matriz
+.PARAMETER Col
+   Cantidad de columnas de la matriz
+.PARAMETER Escalar
+   El escalar con el cual se realiza el producto escalar
 #>
 function productoMatriz($matriz, [int] $fil, [int] $col, [int] $escalar){
     $matRes = New-Object 'Object[,]' $fil,$col
