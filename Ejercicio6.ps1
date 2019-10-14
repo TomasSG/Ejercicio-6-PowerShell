8492a742455e03c473d4a4f88e62dd35ab905332
TomasSG <tomassanchezgrigioni98@gmail.com>
2019-10-14 11:06:37 -0300

    Resuelto la suma de matrices y se planteo el producto escalar

--- Ejercicio6.ps1
+++ Ejercicio6.ps1
@@ -6,9 +6,9 @@
 
 Param (
         [ValidateNotNullOrEmpty()]
-        [parameter(ParameterSetName="Entrada", Mandatory=$true)]
-        [parameter(ParameterSetName="Producto", Mandatory=$false)]
-        [parameter(ParameterSetName="Suma", Mandatory=$false)]
+        [parameter(ParameterSetName="Entrada", Mandatory=$true , Position = 1)]
+        [parameter(ParameterSetName="Producto", Mandatory=$false, Position = 1)]
+        [parameter(ParameterSetName="Suma", Mandatory=$false, Position = 1)]
         [String]
         $entrada,
         [ValidateNotNull()]
@@ -20,6 +20,8 @@ Param (
         [String]
         $suma
         )
+
+
 
 <#
 .SYNOPSIS
@@ -80,6 +82,53 @@ function escribirMatriz([String] $path, $mat, [int] $fil, [int] $col){
     $resultado|Out-File -FilePath "$path"
 }
 
+<#
+.SYNOPSIS
+    Dada dos matrices y sus respectivas filas y columnas, guarda en una nueva matriz el resultado de la suma
+.DESCRIPTION
+    La función no realiza el control de que las dimensionas coincidan entonces se tiene que realizar previamente a su llamada.
+    A partir de las matrices y sus respectivas filas y columnas, se genera una matriz con las dimensiones correspondientes. En la matriz resultado se guarda la suma
+    de las otras dos matrices.
+    La función retorna la matriz resultado, la cantidad de filas y columnas
+.PARAMETER
+    $matriz1 --> La primera matriz a sumar
+    $fil1 --> La cantidad de filas de la primera matriz
+    $col1 --> La cantidad de columnas de la primera matriz
+    $matriz2 --> La segunda matriz a sumar
+    $fil2 --> La cantidad de filas de la segunda matriz
+    $col2 --> La cantidad de columnas de la segunda matriz
+#>
+function sumarMatrices($matriz1,[int] $fil1, [int] $col1, $matriz2, [int] $fil2, [int] $col2){
+    $matRes = New-Object 'Object[,]' $fil1,$col1
+    foreach ( $i in 0..($fil1 -1 ) ){
+        foreach ($j in 0..($col1 - 1) ){
+            $matRes[$i,$j]= $matriz1[$i,$j]+$matriz2[$i,$j] -as [double]
+        }
+    }
+    return $matRes,$fil1,$col1
+}
+
+<#
+.SYNOPSIS
+   Dada una matriz, la cantidad de filas y de columnas y un escalar, se guarda en una nueva matriz el producto escalar.
+.DESCRIPTION
+    A partir de la matriz dada y el escalar, se genera una nueva matriz con sus respectivas dimensiones donde se guarda el resultado del producto escalar
+    La función retorna la matriz resultado, la cantidad de filas y columnas
+.PARAMETER
+   $matriz --> La matriz con la cual se realiza el producto escalar
+   $fil --> Cantidad de filas de la matriz
+   $col --> Cantidad de columnas de la matriz
+   $escalar --> El escalar con el cual se realiza el producto escalar
+#>
+function productoMatriz($matriz, [int] $fil, [int] $col, [double] $escalar){
+    $matRes = New-Object 'Object[,]' $fil,$col
+    foreach ( $i in 0..($fil -1 ) ){
+        foreach ($j in 0..($col - 1) ){
+            $matRes[$i,$j]= $matriz[$i,$j]*$escalar -as [double]
+        }
+    }
+    return $matRes,$fil,$col
+}
 
 
 
@@ -90,19 +139,32 @@ switch($PSBoundParameters.Keys){
                 $vecAux=$entrada.Split("\")
                 $nombreArchivo=$vecAux[$vecAux.Count - 1]
                 $matrizOriginal,$fil,$col = cargarMatriz $entrada
-                escribirMatriz "$directorioScript\salida.$nombreArchivo" $matrizOriginal $fil $col
             } else {
                 Write-Host "La dirección '$entrada' no corresponde a un fichero válido"
                 exit 1
             }    
     }
     "producto"{
-            Write-Host "$producto"
+            if ( ! $matrizOriginal ){
+                Write-Host "Aún no se epsecificó la matriz original"
+                exit 1
+            }
+            $matrizProducto,$filP,$colP = productoMatriz $matrizOriginal,$fil,$col,$producto
+            escribirMatriz "$directorioScript\salida.$nombreArchivo" $matrizProducto $filP $colP
     }
     "suma"{
             if ( Test-Path "$suma" -PathType Leaf  ) {
-                Write-Host "Matriz cargada correctamente de $suma"
-                
+                if ( ! $matrizOriginal ) {
+                    Write-Host "Aún no se especificó la matriz original"
+                    exit 1
+                }
+                $matrizSuma,$filS,$colS = cargarMatriz $suma
+                if( ! $filS -eq $fil -and ! $colS -eq $col ) {
+                    Write-Host "Las dimensiones de las matrices no coinciden"
+                    exit 1
+                }
+                $matrizResultado,$filR,$colR = sumarMatrices $matrizOriginal $fil $col $matrizSuma $filS $colS 
+                escribirMatriz "$directorioScript\salida.$nombreArchivo" $matrizResultado $filR $colR
             } else {
                 Write-Host "La dirección '$suma' no corresponde a un fichero válido"
                 exit 1
